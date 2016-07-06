6429afcd55c9f53f80214ba9b5aa922b47123d9c
Developer <lucas.vasconcelos@elo7.com>
2016-07-06 16:25:36 -0300

    Removendo a necessidade de um ponteiro na função GetDockerVersion e retornando o erro junto com a função

--- src/github.com/elo7/harbor/execute/docker/docker_version.go
+++ src/github.com/elo7/harbor/execute/docker/docker_version.go
@@ -1,30 +1,20 @@
 package docker
 
 import (
-	"fmt"
 	"github.com/hashicorp/go-version"
-	"os"
 	"os/exec"
 	"strings"
 )
 
-func GetDockerVersion(dockerVersion *string) {
-
+func GetDockerVersion() (dockerVersion string, err error) {
 	output, err := exec.Command("docker", "version", "--format='{{.Client.Version}}'").CombinedOutput()
-
-	if err != nil {
-		fmt.Println("Não consegui executar o comando 'docker'. Output: " + string(output))
-		os.Exit(1)
-	} else {
-		*dockerVersion = strings.TrimSpace(string(output))
-	}
+	return strings.TrimSpace(string(output)), err
 }
 
 // Retorna false se o docker for menor que a versão passada
 // Returna true se o docker for maior ou igual que a versão passada
 func CompareDockerVersion(dockerCmpVersion string) bool {
-	var dockerVersion string
-	GetDockerVersion(&dockerVersion)
+	dockerVersion, _ := GetDockerVersion()
 	v1, _ := version.NewVersion(dockerVersion)
 	v2, _ := version.NewVersion(dockerCmpVersion)
 
