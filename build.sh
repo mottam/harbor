3dac02587e66122156d65d05182f8996d158dc70
Beatriz Vieira <beatriznsv@gmail.com>
2017-06-20 15:35:20 -0300

    compress binary

--- build.sh
+++ build.sh
@@ -25,18 +25,22 @@ function execDocker() {
 	docker run --rm -i -v "$(pwd)":/gopath/src/github.com/elo7/harbor -e "GOPATH=/gopath" -w /gopath/src/github.com/elo7/harbor golang:latest sh -c "$1"
 }
 
+function compress() {
+	mkdir gzout
+	mv harbor-v$1-$2-$3 gzout/harbor
+	tar -z -C gzout -cvf harbor_$1_$2_$3.tar.gz harbor
+	rm -rf gzout
+}
+
 
 COMPILE_OS=$1
 COMPILE_ARCH=$2
-VERSION=$3
+
+VERSION=$(grep "const VERSION" main.go | awk '{print substr($NF,2,length($NF)-2)}')
 
 if [ -z "$VERSION" ]; then
-	VERSION=$(grep "const VERSION" main.go | awk '{print substr($NF,2,length($NF)-2)}')
-	
-	if [ -z "$VERSION" ]; then
-		echo "could not find project version"
-		exit 1
-	fi
+	echo "could not find project version"
+	exit 1
 fi
 
 BUILD_CMD="go version && go test ./... && CGO_ENABLED=0 GOOS=$COMPILE_OS GOARCH=$COMPILE_ARCH go build -v -a -installsuffix cgo --ldflags=\"-s\" -o harbor-v$VERSION-$COMPILE_OS-$COMPILE_ARCH ."
@@ -47,3 +51,6 @@ else
     execDocker "$BUILD_CMD"
 fi
 
+if [[ "$OUTPUT" == "gz" ]]; then
+	compress $VERSION $COMPILE_OS $COMPILE_ARCH
+fi
