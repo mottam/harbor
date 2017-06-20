4a56ec0e2986b6dae46b0dcb4ef445d2b1d3954c
Beatriz Vieira <beatriznsv@gmail.com>
2017-06-20 15:06:30 -0300

    script for jenkins, build with and without docker

--- build.sh
+++ build.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 set -e
 
+function checkGoEnv() {
+	if [ -z "$GOPATH" ]; then
+		echo "GOPATH is missing"
+		exit 1
+	fi
+
+	if [[ "$(pwd)" != $GOPATH* ]]; then  
+	  echo "project should be in [$GOPATH]"  
+	  exit 1
+	fi
+
+}
+
+function execLocal() {
+	echo "[local] building harbor v$VERSION for os[$COMPILE_OS] arch[$COMPILE_ARCH]"
+	checkGoEnv
+	eval $1
+}
+
+function execDocker() {
+	echo "[docker] building harbor v$VERSION for os[$COMPILE_OS] arch[$COMPILE_ARCH]"
+	docker run --rm -i -v "$(pwd)":/gopath/src/github.com/elo7/harbor -e "GOPATH=/gopath" -w /gopath/src/github.com/elo7/harbor golang:latest sh -c "$1"
+}
+
+
 COMPILE_OS=$1
 COMPILE_ARCH=$2
-VERSION=$(grep "const VERSION" main.go | awk '{print substr($NF,2,length($NF)-2)}')
+VERSION=$3
+
 if [ -z "$VERSION" ]; then
-	echo "could not find project version"
-	exit 1
+	VERSION=$(grep "const VERSION" main.go | awk '{print substr($NF,2,length($NF)-2)}')
+	
+	if [ -z "$VERSION" ]; then
+		echo "could not find project version"
+		exit 1
+	fi
 fi
 
-echo "building harbor v$VERSION for os[$COMPILE_OS] arch[$COMPILE_ARCH]"
-docker run --rm -i -v "$(pwd)":/gopath/src/github.com/elo7/harbor -e "GOPATH=/gopath" -w /gopath/src/github.com/elo7/harbor golang:latest sh -c "pwd && go version && go test github.com/elo7/harbor/... && CGO_ENABLED=0 GOOS=$COMPILE_OS GOARCH=$COMPILE_ARCH go build -v -a -installsuffix cgo --ldflags=\"-s\" -o harbor-v$VERSION-$COMPILE_OS-$COMPILE_ARCH ."
+BUILD_CMD="go version && go test ./... && CGO_ENABLED=0 GOOS=$COMPILE_OS GOARCH=$COMPILE_ARCH go build -v -a -installsuffix cgo --ldflags=\"-s\" -o harbor-v$VERSION-$COMPILE_OS-$COMPILE_ARCH ."
+GOEXISTS=$(hash go)
+if [ -z "$GOEXISTS" ]; then
+	execLocal "$BUILD_CMD"
+else
+    execDocker "$BUILD_CMD"
+fi
+
